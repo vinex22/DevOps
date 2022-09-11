17930d01a51ba51294405770ab71a7f5a12951c0
Vinay Jain <vinex22@gmail.com>
2022-09-11 16:46:17 +0800

    v

---
+++ Python/test.py
@@ -0,0 +1,13 @@
+calculation_to_unit = 24*60
+name_of_unit = 'Minutes'
+
+
+def days_to_units(days):
+    print(f'{days} days are {days*calculation_to_unit} {name_of_unit}')
+    print('All good!')
+
+days_to_units()
+
+
+
+
